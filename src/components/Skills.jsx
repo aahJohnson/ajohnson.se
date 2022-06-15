8098f25308a1af9208415aaa138d0d5ef95f59e4
aJohnson <johnson9438@gmail.com>
2022-06-15 11:25:28 +0200

    added start of sidebar with personal info

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,36 @@ import React, { Component } from "react";
 import "../style/skills.css";
 
 const Skills = () => {
-  return <div class="skills">Skills</div>;
+  return (
+    <div class="skills">
+      <div class="personal">
+        <img class="profileImg" src=""></img>
+        <h4 class="name">Adam Johnson</h4>
+        <p class="title">Front-end web developer</p>
+      </div>
+      <div class="about">
+        <p class="aboutQ">Residence:</p>
+        <p class="aboutA">Sweden</p>
+        <p class="aboutQ">City:</p>
+        <p class="aboutA">Malmö</p>
+        <p class="aboutQ">Age:</p>
+        <p class="aboutA">27</p>
+      </div>
+      <ul class="languages">
+        <li>100%</li>
+        <li>98%</li>
+        <li>Swedish</li>
+        <li>English</li>
+      </ul>
+      <ul class="skillBars">
+        <li>
+          <p class="skillTitle">HTML</p>
+          <div class="progressBarProgress">85%</div>
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Skills;
