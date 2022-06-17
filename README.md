c4ed60571e8ed92119ad4a8ad434a4ebd4a42ae4
aJohnson <johnson9438@gmail.com>
2022-06-17 17:55:46 +0200

    increased sizing slightly

---
+++ README.md
@@ -0,0 +1,3 @@
+Personal CV for Adam Johnson, created with react.
+
+NOTE: Work in progress, not yet finished!
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App";
+
+// Css
+import "./../style/navbar.css";
+
+const Navbar = () => {
+  return <div class="navbar"></div>;
+};
+
+export default Navbar;
